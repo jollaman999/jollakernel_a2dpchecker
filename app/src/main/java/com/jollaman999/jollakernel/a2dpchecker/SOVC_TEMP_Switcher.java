b4cc020fbc3b58114ab367dc0a110f38a79f350f
jollaman999 <admin@jollaman999.com>
2015-12-24 17:08:24 +0900

    Temporary enable dt2w when playing music

--- app/src/main/java/com/jollaman999/jollakernel/a2dpchecker/SOVC_TEMP_Switcher.java
+++ app/src/main/java/com/jollaman999/jollakernel/a2dpchecker/SOVC_TEMP_Switcher.java
@@ -6,15 +6,25 @@ public class SOVC_TEMP_Switcher {
         Runtime runtime = Runtime.getRuntime();
         try{
             if(on == 1) {
-                String cmd[] = {"/system/bin/sh",
-                                "-c",
-                                "echo 1 | su -c tee /sys/android_touch/scroff_volctr_temp"};
-                runtime.exec(cmd);
+                String cmd1[] = {"/system/bin/sh",
+                            "-c",
+                            "echo 1 | su -c tee /sys/android_touch/scroff_volctr_temp"};
+                runtime.exec(cmd1);
+
+                String cmd2[] = {"/system/bin/sh",
+                            "-c",
+                            "echo 1 | su -c tee /sys/android_touch/doubletap2wake_tmp"};
+                runtime.exec(cmd2);
             } else {
-                String cmd[] = {"/system/bin/sh",
-                        "-c",
-                        "echo 0 | su -c tee /sys/android_touch/scroff_volctr_temp"};
-                runtime.exec(cmd);
+                String cmd1[] = {"/system/bin/sh",
+                            "-c",
+                            "echo 0 | su -c tee /sys/android_touch/scroff_volctr_temp"};
+                runtime.exec(cmd1);
+
+                String cmd2[] = {"/system/bin/sh",
+                            "-c",
+                            "echo 0 | su -c tee /sys/android_touch/doubletap2wake_tmp"};
+                runtime.exec(cmd2);
             }
         } catch (Exception e) {
             e.printStackTrace();
